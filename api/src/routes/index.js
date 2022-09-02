2cfc4c354b24b62bd8258f2e555688ade0fecff5
Nicoovied0 <briannicolasoviedo@hotmail.com>
2022-09-02 01:03:51 -0300

    get db ready

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -4,21 +4,32 @@ const { Character, Occupation } = require('../db.js');
 // Importar todos los routers;
 const router = Router();
 
-const getApi = async() =>{
-  const {data } = await axios.get('https://breakingbadapi.com/api/characters')
-  const info = await data.map( e =>{
+const getApi = async () => {
+  const { data } = await axios.get('https://breakingbadapi.com/api/characters')
+  const info = await data.map(e => {
     return {
       id: e.char_id,
-      name:e.name,
+      name: e.name,
       nickName: e.nickName,
       birthday: e.birthday,
-      image:e.img,
-      status:e.status,
-      occupation:e.occupation.map(e => e)
+      image: e.img,
+      status: e.status,
+      occupation: e.occupation.map(e => e)
     }
-    
   });
   return info
+}
+
+const getDb = async () => {
+  return await Character.findAll({
+    include: {
+      model: Occupation,
+      attributes: ['name'],
+      through: {
+        attributes: []
+      }
+    }
+  })
 }
 
 
