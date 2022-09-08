ecf16b550b0cb4035900207db22b16bd18e5741b
Nicoovied0 <briannicolasoviedo@hotmail.com>
2022-09-07 23:32:32 -0300

    get characters init

---
+++ client/src/redux/actions.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+export const CHARACTERS = "CHARACTERS";
+// export const BYNAME = "BYNAME";
+// export const DETAILS = "DETAILS";
+// export const STATUS = "STATUS";
+// export const BYCREATED = "BYCREATED";
+// export const ORDER = 'ORDER';
+// export const OCCUPATION = "OCCUPATION";
+// export const POST = "POST";
+
+export function getCharacters() {
+  return async function(dispatch) {
+      const res = await axios.get('http://localhost:3001/characters')
+      return dispatch({
+          type: CHARACTERS,
+          payload: res.data
+      }); 
+  };
+};
