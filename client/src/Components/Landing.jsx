2ef5e0d75382de81beab6ccb1f3ff33a9758e69d
Nicoovied0 <briannicolasoviedo@hotmail.com>
2022-09-08 21:58:32 -0300

    landing background

--- client/src/Components/Landing.jsx
+++ client/src/Components/Landing.jsx
@@ -2,11 +2,13 @@ import { Link } from "react-router-dom";
 import '../styles/landing.css';
 const Landing = () => {
   return (
+    <div className="backLanding">
     <div className="LandingPage">
       <h2>Aguante BreakingBad</h2>
       <Link to="/characters">
         <button>WELCOME</button>
       </Link>
+    </div>
     </div>
   );
 };
