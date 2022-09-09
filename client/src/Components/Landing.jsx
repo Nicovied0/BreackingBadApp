926496184a9b57e49bff75c8728d4511af08145b
Nicoovied0 <briannicolasoviedo@hotmail.com>
2022-09-08 22:13:48 -0300

    landing page ready

--- client/src/Components/Landing.jsx
+++ client/src/Components/Landing.jsx
@@ -1,14 +1,14 @@
 import { Link } from "react-router-dom";
-import '../styles/landing.css';
+import "../styles/landing.css";
 const Landing = () => {
   return (
     <div className="backLanding">
-    <div className="LandingPage">
-      <h2>Aguante BreakingBad</h2>
-      <Link to="/characters">
-        <button>WELCOME</button>
-      </Link>
-    </div>
+      <div className="LandingPage">
+        <h2>Breaking Bad Characters</h2>
+        <Link to="/characters">
+          <button className="btnLanding">WELCOME</button>
+        </Link>
+      </div>
     </div>
   );
 };
